095ea38a636d425dca7cc20a13b9400d72ad1c10
Digital Space X <digitalspacex.ofc@gmail.com>
2025-10-23 12:31:53 +0000

    eu quero que tire essa seção "¿Cómo funciona?" (_for element section_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from '@/components/landing/hero';
 import Benefits from '@/components/landing/benefits';
-import HowItWorks from '@/components/landing/how-it-works';
 import Pricing from '@/components/landing/pricing';
 import Bonuses from '@/components/landing/bonuses';
 import Testimonials from '@/components/landing/testimonials';
@@ -12,7 +11,6 @@ export default function LandingPage() {
       <main className="flex-1">
         <Hero />
         <Benefits />
-        <HowItWorks />
         <Pricing />
         <Bonuses />
         <Testimonials />
