61430b04c0ec6dee8df0b877a93371b01474d59c
Digital Space X <digitalspacex.ofc@gmail.com>
2025-10-23 12:28:14 +0000

    eu quero que tire essa seção (_for element section_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Hero from '@/components/landing/hero';
-import Partners from '@/components/landing/partners';
 import Benefits from '@/components/landing/benefits';
 import HowItWorks from '@/components/landing/how-it-works';
 import Pricing from '@/components/landing/pricing';
@@ -12,7 +11,6 @@ export default function LandingPage() {
     <div className="flex min-h-dvh flex-col">
       <main className="flex-1">
         <Hero />
-        <Partners />
         <Benefits />
         <HowItWorks />
         <Pricing />
