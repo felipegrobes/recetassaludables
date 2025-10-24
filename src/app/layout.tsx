aaf55baec69129ea47ea27e85a74988520adadc4
Digital Space X <digitalspacex.ofc@gmail.com>
2025-10-24 00:29:07 +0000

    instala o codigo pixel do facebook no site, instala na head. instala do

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins, Inter } from 'next/font/google';
 import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
 import { cn } from '@/lib/utils';
+import Script from 'next/script';
 
 const fontPoppins = Poppins({
   subsets: ['latin'],
@@ -28,7 +29,31 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es">
+    <html lang="es" className="scroll-smooth">
+      <head>
+        <Script id="meta-pixel-code" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1121882016823728');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1121882016823728&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
       <body
         className={cn(
           'min-h-screen bg-background font-body antialiased',
